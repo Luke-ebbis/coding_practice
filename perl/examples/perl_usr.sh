6ec82cba689c665da26f98b32383fe20db0d8b72
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-01-30 22:59:51 +0100

    moved files

---
+++ perl/examples/perl_usr.sh
@@ -0,0 +1,6 @@
+echo "atgcatgcggagttgcaggctttggagg" | perl shannon_entropy.pl 
+echo "asdsadsadsadd" | perl shannon_entropy.pl 
+echo "aaaaaaaaaaaaa" | perl shannon_entropy.pl 
+echo "asdsadsadsadd" | perl shannon_entropy.pl 
+echo "asdsadsadsadd" | perl shannon_entropy.pl 
+
