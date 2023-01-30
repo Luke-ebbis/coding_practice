cdfefaad49c87482f3a4d02a9392497730dd93c4
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-01-30 23:03:11 +0100

    removed un needed files

--- perl/perl_usr.sh
+++
@@ -1,6 +0,0 @@
-echo "atgcatgcggagttgcaggctttggagg" | perl shannon_entropy.pl 
-echo "asdsadsadsadd" | perl shannon_entropy.pl 
-echo "aaaaaaaaaaaaa" | perl shannon_entropy.pl 
-echo "asdsadsadsadd" | perl shannon_entropy.pl 
-echo "asdsadsadsadd" | perl shannon_entropy.pl 
-
